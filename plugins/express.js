0de4a4173016439f38c1dadc12ced4563590d6b7
Jeremy Mwangelwa <jeremy@nizamedia.com>
2022-11-27 10:38:55 -0500

    Commit nuxt files

---
+++ plugins/express.js
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+const port = 3003;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+console.log("PORT: ", process.env.PORT);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
