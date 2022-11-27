bd0031e638a088a8f997300894d88da9e0b098e7
Jeremy Mwangelwa <jeremy@nizamedia.com>
2022-11-27 10:45:54 -0500

    Add export to plugin

--- plugins/express.js
+++ plugins/express.js
@@ -11,3 +11,11 @@ console.log("PORT: ", process.env.PORT);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+export default defineNuxtPlugin(() => {
+  return {
+    provide: {
+      auth: "Express ready",
+    },
+  };
+});
